212e09ea4a08d3922dba3a307cb3d003d19b9b38
Lucas Daniel Veríssimo <lucasdaniel.verissimo@gmail.com>
2020-01-17 09:49:45 -0300

    Adição dos scripts da grid de movimentação

---
+++ Icarus_Maze/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridMovement : MonoBehaviour
+{
+    public Vector3 GetNewPosition(Vector3 old_pos, int xDir, int yDir)
+    {
+        Tilemap tlmap = GetComponent<Tilemap>();
+        Vector3Int cell = tlmap.WorldToCell(old_pos);
+        cell.x = cell.x + xDir;
+        cell.y = cell.y + yDir;
+        if(tlmap.GetTile(cell) != null)
+        {
+            return tlmap.GetCellCenterWorld(cell);
+        }
+        else
+        {
+            return new Vector3();
+        }
+    }
+}
